8b39080b1d6f635a0cdd7067761255251b792ab3
Lzap10 <lionelnkoum@gmail.com>
2022-02-03 13:13:45 +0100

    ajout de l'instance personne dans la classe principale

--- src/main/java/mypackage/Mymain.java
+++ src/main/java/mypackage/Mymain.java
@@ -1,4 +1,6 @@
 package mypackage;
+import mypackage.Personne;
+import mypackage.Montre;
 
 
 public class Mymain {
@@ -6,6 +8,13 @@ public class Mymain {
     public  static void main(String[] arg){
         Montre Rolex = new Montre(23,58);
         Montre FakeRolex = new Montre(Rolex);
+        Personne moi = new Personne("Nkoum");
+        Personne toi = new Personne("Lionel");
+        toi.porteMontre(FakeRolex);
+        moi.porteMontre(Rolex);
+        moi.demanderHeure(toi);
+        moi.Pmontre.avance();
+        toi.demanderHeure(moi);
 
     }
 }
