93a8784e5d65c5126f2214935ec732081a2655a1
Mranderson6 <bigshow.stephane@gmail.com>
2022-02-03 09:59:25 +0100

    Premier commit avec ecriture de la classe montre et instanciation dans la classe main

---
+++ src/main/java/mypackage/Mymain.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+
+public class Mymain {
+
+    public  static void main(String[] arg){
+        Montre Rolex = new Montre(23,58);
+        Montre FakeRolex = new Montre(Rolex);
+
+    }
+}
